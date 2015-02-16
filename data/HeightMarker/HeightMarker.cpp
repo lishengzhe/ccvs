00770e63423f16772b099e37771d87a0879b17c3
Shengzhe Li <lishengzhe@gmail.com>
2015-02-16 15:49:52 +0900

    Update HeightMarker.cpp

--- data/HeightMarker/HeightMarker.cpp
+++ data/HeightMarker/HeightMarker.cpp
@@ -27,8 +27,6 @@ published by the Free Software Foundation.
 #include <fstream>
 #include <stdlib.h>
 
-#include "VinUtils.h"
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/video/video.hpp"
